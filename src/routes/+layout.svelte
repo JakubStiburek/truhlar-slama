40e9ca13dcc21588327ebb71a88db5fd37219052
Jakub Stibůrek <alcolp96@gmail.com>
2022-12-19 11:38:06 +0100

    page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 </script>
 
 <svelte.head>
+    <title>Truhlář Sláma</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400;700&display=swap');
 
