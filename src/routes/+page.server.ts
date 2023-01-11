42037b4373074ee130278ab3f324e8ef1077be4b
Jakub Stibůrek <alcolp96@gmail.com>
2023-01-11 09:29:39 +0100

    resize images for target device

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,8 @@
 import { Cloudinary } from "@cloudinary/url-gen";
 import { CLOUDINARY_KEY, CLOUDINARY_NAME, CLOUDINARY_SECRET } from "$env/static/private";
-import { format } from "@cloudinary/url-gen/actions/delivery";
+import { Delivery } from "@cloudinary/url-gen/actions/delivery";
 import { Format } from "@cloudinary/url-gen/qualifiers";
+import { scale } from "@cloudinary/url-gen/actions/resize";
 
 const cloudinary = new Cloudinary({
     cloud: {
@@ -13,19 +14,48 @@ const cloudinary = new Cloudinary({
 
 export async function load() {
     const stool = cloudinary.image('truhlarslama.cz/stool_xgbssq.jpg')
-        .delivery(format(Format.webp()));
+        .delivery(Delivery.format(Format.webp()))
+        .resize(scale().width(540).height(720));
     const stoolDetail = cloudinary.image('truhlarslama.cz/stool-detail_ckws3b.jpg')
-        .delivery(format(Format.webp()));
+        .delivery(Delivery.format(Format.webp()))
+        .resize(scale().width(540).height(720));
     const stoolDetailFull = cloudinary.image('truhlarslama.cz/stool-detail-full_dp9ewk.jpg')
-        .delivery(format(Format.webp()));
+        .delivery(Delivery.format(Format.webp()))
+        .resize(scale().width(540).height(720));
     const stoolWithPerson = cloudinary.image('truhlarslama.cz/stool-with-person_jzgaq4.jpg')
-        .delivery(format(Format.webp()));
+        .delivery(Delivery.format(Format.webp()))
+        .resize(scale().width(540).height(720));
     const nightStand = cloudinary.image('truhlarslama.cz/night-stand_zc4xnb.jpg')
-        .delivery(format(Format.webp()));
+        .delivery(Delivery.format(Format.webp()))
+        .resize(scale().width(540).height(720));
     const nightStandDetail = cloudinary.image('truhlarslama.cz/night-stand-detail_ipwvsh.jpg')
-        .delivery(format(Format.webp()));
+        .delivery(Delivery.format(Format.webp()))
+        .resize(scale().width(540).height(720));
     const nightStandOpen = cloudinary.image('truhlarslama.cz/night-stand-open_zujpif.jpg')
-        .delivery(format(Format.webp()));
+        .delivery(Delivery.format(Format.webp()))
+        .resize(scale().width(540).height(720));
+
+    const stoolMobile = cloudinary.image('truhlarslama.cz/stool_xgbssq.jpg')
+        .delivery(Delivery.format(Format.webp()))
+        .resize(scale().width(323).height(430.66));
+    const stoolDetailMobile = cloudinary.image('truhlarslama.cz/stool-detail_ckws3b.jpg')
+        .delivery(Delivery.format(Format.webp()))
+        .resize(scale().width(323).height(430.66));
+    const stoolDetailFullMobile = cloudinary.image('truhlarslama.cz/stool-detail-full_dp9ewk.jpg')
+        .delivery(Delivery.format(Format.webp()))
+        .resize(scale().width(323).height(430.66));
+    const stoolWithPersonMobile = cloudinary.image('truhlarslama.cz/stool-with-person_jzgaq4.jpg')
+        .delivery(Delivery.format(Format.webp()))
+        .resize(scale().width(323).height(430.66));
+    const nightStandMobile = cloudinary.image('truhlarslama.cz/night-stand_zc4xnb.jpg')
+        .delivery(Delivery.format(Format.webp()))
+        .resize(scale().width(323).height(430.66));
+    const nightStandDetailMobile = cloudinary.image('truhlarslama.cz/night-stand-detail_ipwvsh.jpg')
+        .delivery(Delivery.format(Format.webp()))
+        .resize(scale().width(323).height(430.66));
+    const nightStandOpenMobile = cloudinary.image('truhlarslama.cz/night-stand-open_zujpif.jpg')
+        .delivery(Delivery.format(Format.webp()))
+        .resize(scale().width(323).height(430.66));
 
     return {
         stool: stool.toURL(),
@@ -35,5 +65,13 @@ export async function load() {
         nightStand: nightStand.toURL(),
         nightStandDetail: nightStandDetail.toURL(),
         nightStandOpen: nightStandOpen.toURL(),
+
+        stoolMobile: stool.toURL(),
+        stoolDetailMobile: stoolDetail.toURL(),
+        stoolDetailFullMobile: stoolDetailFull.toURL(),
+        stoolWithPersonMobile: stoolWithPerson.toURL(),
+        nightStandMobile: nightStand.toURL(),
+        nightStandDetailMobile: nightStandDetail.toURL(),
+        nightStandOpenMobile: nightStandOpen.toURL(),
     };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,21 @@
 <script>
     import Contacts from "../components/Contacts.svelte";
+    import {browser} from '$app/environment';
 
     export const prerender = true;
 
     export let data;
+
+    let mobile = false;
+
+    if (browser) {
+        mobile = window.matchMedia("(max-width: 640px)").matches;
+    }
 </script>
 
 <div class="grid 2xl:grid-cols-2 grid-cols-1 justify-center w-max mx-auto mt-3">
     <div class="flex flex-col justify-center items-center mx-auto rounded w-max h-max img">
-        <img class="sm:w-[540px] w-[323px]" src={data.stoolDetail} alt="night stand">
+        <img class="sm:w-[540px] w-[323px]" src={mobile ? data.stoolDetailMobile : data.stoolDetail} alt="night stand">
     </div>
     <div class="flex flex-col justify-center items-start sm:p-10 p-6">
         <h2 class="sm:text-3xl text-2xl text-left pb-3 text-black">
@@ -26,25 +33,29 @@
         <Contacts/>
     </div>
     <div class="flex flex-col justify-center items-center mx-auto rounded w-max h-max img">
-        <img class="sm:w-[540px] w-[323px]" src={data.nightStandDetail} alt="night stand wip">
+        <img class="sm:w-[540px] w-[323px]" src={mobile ? data.nightStandDetailMobile : data.nightStandDetail}
+             alt="night stand wip">
     </div>
     <div class="flex flex-col justify-center items-center mx-auto rounded w-max h-max img">
-        <img class="sm:w-[540px] w-[323px]" src={data.nightStand} alt="soap bars">
+        <img class="sm:w-[540px] w-[323px]" src={mobile ? data.nightStandMobile : data.nightStand} alt="soap bars">
     </div>
     <div class="flex flex-col justify-center items-center mx-auto rounded w-max h-max img">
-        <img class="sm:w-[540px] w-[323px]" src={data.nightStandOpen} alt="bread box">
+        <img class="sm:w-[540px] w-[323px]" src={mobile ? data.nightStandOpenMobile : data.nightStandOpen}
+             alt="bread box">
     </div>
     <div class="flex flex-col justify-center items-center mx-auto rounded w-max h-max img">
-        <img class="sm:w-[540px] w-[323px]" src={data.stool} alt="lid">
+        <img class="sm:w-[540px] w-[323px]" src={mobile ? data.stoolMobile : data.stool} alt="lid">
     </div>
     <div class="flex flex-col justify-center items-center mx-auto rounded w-max h-max img">
-        <img class="sm:w-[540px] w-[323px]" src={data.stoolDetailFull} alt="shave stand">
+        <img class="sm:w-[540px] w-[323px]" src={mobile ? data.stoolDetailFullMobile : data.stoolDetailFull}
+             alt="shave stand">
     </div>
     <div class="flex flex-col justify-center items-center mx-auto rounded w-max h-max img">
-        <img class="sm:w-[540px] w-[323px]" src={data.stoolWithPerson} alt="shave stand">
+        <img class="sm:w-[540px] w-[323px]" src={mobile ? data.stoolWithPersonMobile : data.stoolWithPerson}
+             alt="shave stand">
     </div>
     <div class="flex flex-col justify-center items-center mx-auto rounded w-max h-max img">
-        <img class="sm:w-[540px] w-[323px]" src={data.stoolDetail} alt="night stand">
+        <img class="sm:w-[540px] w-[323px]" src={mobile ? data.stoolDetailMobile : data.stoolDetail} alt="night stand">
     </div>
 </div>
 
