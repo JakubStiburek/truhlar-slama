426658d4315f46837413bf34de6b5b12a92892e4
Jakub Stibůrek <alcolp96@gmail.com>
2023-01-11 10:24:42 +0100

    explicit image size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,10 @@
 
 <div class="grid 2xl:grid-cols-2 grid-cols-1 justify-center w-max mx-auto mt-3">
     <div class="flex flex-col justify-center items-center mx-auto rounded w-max h-max img">
-        <img class="sm:w-[540px] w-[323px]" src={mobile ? data.stoolDetailMobile : data.stoolDetail} alt="night stand">
+        <img class="sm:w-[540px] w-[323px]"
+             width={mobile ? "323" : "540"}
+             height={mobile ? "430.66" : "720"}
+             src={mobile ? data.stoolDetailMobile : data.stoolDetail} alt="night stand">
     </div>
     <div class="flex flex-col justify-center items-start sm:p-10 p-6">
         <h2 class="sm:text-3xl text-2xl text-left pb-3 text-black">
@@ -33,29 +36,50 @@
         <Contacts/>
     </div>
     <div class="flex flex-col justify-center items-center mx-auto rounded w-max h-max img">
-        <img class="sm:w-[540px] w-[323px]" src={mobile ? data.nightStandDetailMobile : data.nightStandDetail}
+        <img class="sm:w-[540px] w-[323px]"
+             width={mobile ? "323" : "540"}
+             height={mobile ? "430.66" : "720"}
+             src={mobile ? data.nightStandDetailMobile : data.nightStandDetail}
              alt="night stand wip">
     </div>
     <div class="flex flex-col justify-center items-center mx-auto rounded w-max h-max img">
-        <img class="sm:w-[540px] w-[323px]" src={mobile ? data.nightStandMobile : data.nightStand} alt="soap bars">
+        <img class="sm:w-[540px] w-[323px]"
+             width={mobile ? "323" : "540"}
+             height={mobile ? "430.66" : "720"}
+             src={mobile ? data.nightStandMobile : data.nightStand} alt="soap bars">
     </div>
     <div class="flex flex-col justify-center items-center mx-auto rounded w-max h-max img">
-        <img class="sm:w-[540px] w-[323px]" src={mobile ? data.nightStandOpenMobile : data.nightStandOpen}
+        <img class="sm:w-[540px] w-[323px]"
+             width={mobile ? "323" : "540"}
+             height={mobile ? "430.66" : "720"}
+             src={mobile ? data.nightStandOpenMobile : data.nightStandOpen}
              alt="bread box">
     </div>
     <div class="flex flex-col justify-center items-center mx-auto rounded w-max h-max img">
-        <img class="sm:w-[540px] w-[323px]" src={mobile ? data.stoolMobile : data.stool} alt="lid">
+        <img class="sm:w-[540px] w-[323px]"
+             width={mobile ? "323" : "540"} src={mobile ? data.stoolMobile : data.stool}
+             height={mobile ? "430.66" : "720"}
+             alt="lid">
     </div>
     <div class="flex flex-col justify-center items-center mx-auto rounded w-max h-max img">
-        <img class="sm:w-[540px] w-[323px]" src={mobile ? data.stoolDetailFullMobile : data.stoolDetailFull}
+        <img class="sm:w-[540px] w-[323px]"
+             width={mobile ? "323" : "540"}
+             height={mobile ? "430.66" : "720"}
+             src={mobile ? data.stoolDetailFullMobile : data.stoolDetailFull}
              alt="shave stand">
     </div>
     <div class="flex flex-col justify-center items-center mx-auto rounded w-max h-max img">
-        <img class="sm:w-[540px] w-[323px]" src={mobile ? data.stoolWithPersonMobile : data.stoolWithPerson}
+        <img class="sm:w-[540px] w-[323px]"
+             width={mobile ? "323" : "540"}
+             height={mobile ? "430.66" : "720"}
+             src={mobile ? data.stoolWithPersonMobile : data.stoolWithPerson}
              alt="shave stand">
     </div>
     <div class="flex flex-col justify-center items-center mx-auto rounded w-max h-max img">
-        <img class="sm:w-[540px] w-[323px]" src={mobile ? data.stoolDetailMobile : data.stoolDetail} alt="night stand">
+        <img class="sm:w-[540px] w-[323px]"
+             width={mobile ? "323" : "540"}
+             height={mobile ? "430.66" : "720"}
+             src={mobile ? data.stoolDetailMobile : data.stoolDetail} alt="night stand">
     </div>
 </div>
 
