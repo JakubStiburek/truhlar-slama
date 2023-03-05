6c11252e130c3605316c919b21692c4b0d71d0b2
Jakub Stibůrek <jakub@stiburek.dev>
2023-03-05 16:32:28 +0100

    404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<div class="text-black flex flex-col justify-center items-center p-5">
+    <p class="text-2xl">
+      404
+    </p>
+    <p class="text-2xl">
+        Stránka nenalezena.
+    </p>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,30 +20,34 @@
     </style>
 </svelte.head>
 
-<div class='dark-background'>
-    <div class='flex justify-start items-center mx-5 gap-5'>
-        <div class='w-max h-max'>
-            <Logo src={data.logoSrc}/>
-        </div>
-        <div>
-            <a href='/'>
-                <p class='sm:text-5xl text-3xl'>
-                    Truhlář Sláma
-                </p>
-            </a>
+<div class="h-screen flex flex-col justify-between">
+    <div class='dark-background'>
+        <div class='flex justify-start items-center mx-5 gap-5'>
+            <div class='w-max h-max'>
+                <Logo src={data.logoSrc}/>
+            </div>
+            <div>
+                <a href='/'>
+                    <p class='sm:text-5xl text-3xl'>
+                        Truhlář Sláma
+                    </p>
+                </a>
+            </div>
         </div>
     </div>
-</div>
 
-<slot/>
+    <div class="mb-auto mx-auto">
+        <slot/>
+    </div>
 
-<div class='h-max flex sm:flex-row flex-col justify-center items-center mx-auto dark-background'>
-    <div>
-        <div class="p-5 border-b border-solid border-t-teal-100 ">
-            <Contacts/>
-        </div>
-        <div class="p-5 hover:text-blue-400">
-            <a href="https://www.linkedin.com/in/jakubstiburekdev/">Created by Jakub Stibůrek</a>
+    <div class='flex flex-col justify-center items-center dark-background'>
+        <div class="w-max h-max">
+            <div class="p-5 border-b border-solid border-t-teal-100 ">
+                <Contacts/>
+            </div>
+            <div class="p-5 hover:text-blue-400">
+                <a href="https://www.linkedin.com/in/jakubstiburekdev/">Created by Jakub Stibůrek</a>
+            </div>
         </div>
     </div>
 </div>
