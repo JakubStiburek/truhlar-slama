63c7e62f366d9346cba241c391092ca549ccd398
Jakub Stibůrek <jakub@stiburek.dev>
2023-05-04 16:47:27 +0200

    feat: new instagram handle

--- src/components/Contacts.svelte
+++ src/components/Contacts.svelte
@@ -1,13 +1,13 @@
 <script>
     const mail = "ondrej.slama@truhlarslama.cz";
     const tel = "608334824";
-    const insta = "https://www.instagram.com/truhlar.slama/";
+    const insta = "https://www.instagram.com/slamuv_truhlarsky_koutek/";
 </script>
 
 <ul class="list-none">
     <li class="py-1.5 hover:text-blue-400"><a
             href={`tel:${tel}`}>{`+420 ${tel.substring(0, 3)} ${tel.substring(3, 6)} ${tel.substring(6, 9)}`}</a></li>
     <li class="py-1.5 hover:text-blue-400"><a href={`mailto:${mail}`}>{mail}</a></li>
-    <li class="py-1.5 hover:text-blue-400"><a href={insta}>IG: @truhlar.slama</a>
+    <li class="py-1.5 hover:text-blue-400"><a href={insta}>IG: @slamuv_truhlarsky_koutek</a>
     </li>
 </ul>
