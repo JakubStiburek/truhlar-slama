325078aa9c73f2e01cb28803c77d56ac7034d3ef
Jakub Stibůrek <jakub@stiburek.dev>
2023-05-06 15:34:21 +0200

    wip

--- src/routes/obchod/+page.svelte
+++ src/routes/obchod/+page.svelte
@@ -14,32 +14,32 @@
 <div class="mx-auto flex flex-col items-center justify-center p-5 sm:max-w-[1240px] max-w-[612px]">
     <h1 class="text-4xl">Obchod</h1>
     <p class="text-center text-2xl">Prohlédněte si zboží připravené k odběru nebo kousky, které již mají majitele. Objednávejte telefonicky nebo emailem.</p>
-    <div class="mx-auto mt-3 grid w-max grid-cols-1 justify-center gap-5 2xl:grid-cols-2">
-        <Item
-                src={mobile ? data.stoolMobile : data.stool}
-                title="Stolička"
-                description="Trojnožka vyrobená z dubového masívu. Ošetřená olejem."
-                price=2800
-                available=true
-                amount=3
-                mobile={mobile}
-        />
-        <Item
-                src={mobile ? data.stoolMobile : data.stool}
-                title="Stolička typ 3"
-                description="Trojnožka vyrobená z dubového masívu. Ošetřená olejem."
-                price=3000
-                available=true
-                amount=15
-                mobile={mobile}
-        />
-        <Item
-                src={mobile ? data.stoolMobile : data.stool}
-                title="Stolička typ 2"
-                description="Trojnožka vyrobená z dubového masívu. Ošetřená olejem."
-                price=2800
-                amount=3
-                mobile={mobile}
-        />
-    </div>
+<!--    <div class="mx-auto mt-3 grid w-max grid-cols-1 justify-center gap-5 2xl:grid-cols-2">-->
+<!--        <Item-->
+<!--                src={mobile ? data.stoolMobile : data.stool}-->
+<!--                title="Stolička"-->
+<!--                description="Trojnožka vyrobená z dubového masívu. Ošetřená olejem."-->
+<!--                price=2800-->
+<!--                available=true-->
+<!--                amount=3-->
+<!--                mobile={mobile}-->
+<!--        />-->
+<!--        <Item-->
+<!--                src={mobile ? data.stoolMobile : data.stool}-->
+<!--                title="Stolička typ 3"-->
+<!--                description="Trojnožka vyrobená z dubového masívu. Ošetřená olejem."-->
+<!--                price=3000-->
+<!--                available=true-->
+<!--                amount=15-->
+<!--                mobile={mobile}-->
+<!--        />-->
+<!--        <Item-->
+<!--                src={mobile ? data.stoolMobile : data.stool}-->
+<!--                title="Stolička typ 2"-->
+<!--                description="Trojnožka vyrobená z dubového masívu. Ošetřená olejem."-->
+<!--                price=2800-->
+<!--                amount=3-->
+<!--                mobile={mobile}-->
+<!--        />-->
+<!--    </div>-->
 </div>
